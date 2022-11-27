19f225a447eb516803abee3689cd91d7c524101f
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-11-27 20:14:38 +0100

    king end

--- Assets/Scripts/King.cs
+++ Assets/Scripts/King.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class King : MonoBehaviour
+public class King : Piece
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        
+        if (BoardManager.Instance.PlayerTurn == PlayerIndex)
+        {
+            PieceMovement();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void PieceMovement()
     {
-        
+        BoardManager.Instance.SelectedPiece = this;
+        BoardManager.Instance.ResetMoves();
+        row = CurrentTileIndex / BoardManager.Instance.BoardSize;
+        column = CurrentTileIndex % BoardManager.Instance.BoardSize;
+        int tileIndexMovement;
+
+        // top line
+        if(row-1>=0)
+        {
+            if(column+1 < BoardManager.Instance.BoardSize)
+            {
+                tileIndexMovement = column + 1 + (row-1) * BoardManager.Instance.BoardSize;
+                MovementOrAttack(tileIndexMovement);
+            }
+            if (column - 1 >= 0)
+            {
+                tileIndexMovement = column - 1 + (row-1) * BoardManager.Instance.BoardSize;
+                MovementOrAttack(tileIndexMovement);
+            }
+            tileIndexMovement = column + (row-1)* BoardManager.Instance.BoardSize;
+            MovementOrAttack(tileIndexMovement);
+        }
+
+        // bot line
+        if (row + 1 < BoardManager.Instance.BoardSize)
+        {
+            if (column + 1 < BoardManager.Instance.BoardSize)
+            {
+                tileIndexMovement = column + 1 + (row+1) * BoardManager.Instance.BoardSize;
+                MovementOrAttack(tileIndexMovement);
+            }
+            if (column - 1 >= 0)
+            {
+                tileIndexMovement = column - 1 + (row+1) * BoardManager.Instance.BoardSize;
+                MovementOrAttack(tileIndexMovement);
+            }
+            tileIndexMovement = column + (row + 1) * BoardManager.Instance.BoardSize;
+            MovementOrAttack(tileIndexMovement);
+        }
+
+        // left move & right move
+        if (column + 1 < BoardManager.Instance.BoardSize)
+        {
+            tileIndexMovement = column + 1 + (row) * BoardManager.Instance.BoardSize;
+            MovementOrAttack(tileIndexMovement);
+        }
+        if (column - 1 >= 0)
+        {
+            tileIndexMovement = column - 1 + (row) * BoardManager.Instance.BoardSize;
+            MovementOrAttack(tileIndexMovement);
+        }
+
     }
 }
